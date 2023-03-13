f6c7dde7ca2e609eb57638b488d4f822c01b5ada
troutshark6 <127745400+troutshark6@users.noreply.github.com>
2023-03-13 15:48:14 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero = 10;
+    double numerolungo = 69888666;
+    float numeroconlavirgola = 6.969;
+    char carattere = 'g';
+
+    printf("il numero è: %d\n", numero);
+    printf("numerolungo è: %f, numeroconlavirgola è: %f\n", numerolungo, numeroconlavirgola);
+    printf("il carattere è %c\n", carattere);
+}
