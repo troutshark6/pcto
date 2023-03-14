5a8351643f999346feef1f19cc416ba4e8500218
troutshark6 <127745400+troutshark6@users.noreply.github.com>
2023-03-14 10:55:11 +0100

    Add files via upload

---
+++ inizio.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int età;
+    char sesso; 
+    printf("Quanti anni hai?\n");
+    scanf(" %d", &età);
+    printf("I tuoi anni sono: %d ", età );
+
+    printf("Sesso?:\n Tanto | Poco: ");
+    scanf(" %c", sesso);
+    printf("Il tuo sesso è: %d ", sesso);
+
+
+
+
+}
