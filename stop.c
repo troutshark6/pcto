b6d98cdba135f27420dc4e43e7ade48d83d12079
troutshark6 <127745400+troutshark6@users.noreply.github.com>
2023-03-16 16:20:47 +0100

    Add files via upload

---
+++ stop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    char stringa[]="";
+    int i = 0;
+    while (1 > 0)
+    {
+            printf("Scrivi una stringa\n");
+    scanf(" %s", stringa);
+    printf(" %s\n", stringa);
+
+    if (stringa[0] == 'S' && stringa[1] == 'T' && stringa[2] == 'O' && stringa[3] == 'P');
+    {
+        break; 
+    }
+    }
+}   
